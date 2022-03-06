c01264f14bbcb767f91386d207c704f2c723f337
Chia9666 <kp.chia9666@gmail.com>
2022-03-06 22:51:31 +0800

    Done 02, 03 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  return array.filter((arrayItem) => !args.includes(arrayItem));
+/*                   ^^ filter out the arrayItem that are included in args. 
+We have an array [1, 2, 3, 4]
+        ...args a rest parameter like an array [2, 3]
+We filter the array from the items in in args array
+are the [2, 3] in the args included array[1, 2 , 3, 4]?
+if yes, remove them for eg. 1
+(arrayItem) => !false <-(arrayItem not includes in args)
+(arrayItem) => true, passes condition so will be put into new array
 
-};
+*/
 
+}
+console.log(removeFromArray([1, 2, 3, 4],2,3))
 // Do not edit below this line
 module.exports = removeFromArray;
