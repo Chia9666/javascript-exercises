f2d75f8ee2e26d9cc06968fb8a86b5de3694be0f
Chia9666 <kp.chia9666@gmail.com>
2022-03-07 18:45:37 +0800

    latest until 09

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  const maxNum = Math.max(a, b);
+  const minNum = Math.min(a, b);
 
+  //initializer is sum
+  let sum = 0;
+  for (let i = minNum; i <= maxNum; i++) {
+    sum += i;
+  }
+  return sum;
+  //Better Algo
+  /*  return ((maxNum - minNum + 1) * (minNum + maxNum)) / 2; */
 };
 
 // Do not edit below this line
