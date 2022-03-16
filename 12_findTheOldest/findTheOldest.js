27f2979375225f30eea57ae648a1462b009f6f49
Chia9666 <kp.chia9666@gmail.com>
2022-03-16 20:38:17 +0800

    "Finished exercises till calculator

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce(function (previousPerson, currentPerson) {
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    const previousAge = getAge(
+      previousPerson.yearOfBirth,
+      previousPerson.yearOfDeath
+    );
 
+    return previousAge < currentAge ? currentPerson : previousPerson;
+  });
+
+  function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
